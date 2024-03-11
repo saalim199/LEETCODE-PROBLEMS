7097e8ea74f577fc0b4a359d65b2f43b61c50c5c
Md Saalim <113614361+saalim199@users.noreply.github.com>
2024-03-11 18:58:10 +0530

    Time: 7 ms (53.63%), Space: 16.7 MB (52.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
+    void rec(int i,int j,vector<vector<int>>& image,int old_color,int new_color){
+        if(i<0 || j<0 || i>=image.size() || j>=image[0].size()
+         || image[i][j]!=old_color || image[i][j]==new_color){
+             return;
+         }
+         image[i][j]=new_color;
+         rec(i-1,j,image,old_color,new_color);
+         rec(i+1,j,image,old_color,new_color);
+         rec(i,j+1,image,old_color,new_color);
+         rec(i,j-1,image,old_color,new_color);
+    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
-        queue<pair<int,int>> q;
-        int oldc=image[sr][sc];
-        q.push({sr,sc});
-        image[sr][sc]=color;
-        int drow[4]={-1,0,1,0};
-        int dcol[4]={0,1,0,-1};
-        while(!q.empty()){
-            int x=q.front().first;
-            int y=q.front().second;
-            q.pop();
-            // cout<<x<<" "<<y<<endl;
-            for(int i=0;i<4;i++){
-                int r=x+drow[i];
-                int c=y+dcol[i];
-                if(r>=0 && c>=0 && r<n && c<m && image[r][c]!=color && image[r][c]==oldc){
-                    image[r][c]=color;
-                    q.push({r,c});
-                }
-            }
-        }
+        // int n=image.size();
+        // int m=image[0].size();
+        // queue<pair<int,int>> q;
+        // int oldc=image[sr][sc];
+        // q.push({sr,sc});
+        // image[sr][sc]=color;
+        // int drow[4]={-1,0,1,0};
+        // int dcol[4]={0,1,0,-1};
+        // while(!q.empty()){
+        //     int x=q.front().first;
+        //     int y=q.front().second;
+        //     q.pop();
+        //     // cout<<x<<" "<<y<<endl;
+        //     for(int i=0;i<4;i++){
+        //         int r=x+drow[i];
+        //         int c=y+dcol[i];
+        //         if(r>=0 && c>=0 && r<n && c<m && image[r][c]!=color && image[r][c]==oldc){
+        //             image[r][c]=color;
+        //             q.push({r,c});
+        //         }
+        //     }
+        // }
+
+        /// RECURSIVE SOLUTION
+
+        rec(sr,sc,image,image[sr][sc],color);
         return image;
     }
 };
