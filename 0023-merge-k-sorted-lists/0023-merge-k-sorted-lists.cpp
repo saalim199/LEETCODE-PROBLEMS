95106f88e2ea960b8878cb6a2ccc40fef9fe87fd
Md Saalim <113614361+saalim199@users.noreply.github.com>
2024-03-14 14:54:05 +0530

    Time: 170 ms (14.61%), Space: 17.1 MB (35.33%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* solve(ListNode *h1,ListNode *h2){
+        if(!h1){
+            return h2;
+        }
+        else if(!h2){
+            return h1;
+        }
+        if(h1->val > h2->val){
+            ListNode *temp=h2->next;
+            h2->next=NULL;
+            h2->next=solve(h1,temp);
+            return h2;
+        }else{
+            ListNode *temp=h1->next;
+            h1->next=NULL;
+            h1->next=solve(temp,h2);
+            return h1;
+        }
+    }
+    // ListNode* merge(vector<ListNode*>& lists,int start,int end){
+    //     if(start>end) return null;
+    //     if(start==end) return lists[end];
+
+    // }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        if(lists.size()==1) return lists[0];
+        ListNode *head=solve(lists[0],lists[1]);
+        for(int i=2;i<lists.size();i++)
+            head=solve(head,lists[i]);
+        return head;
+    }
+
+};
