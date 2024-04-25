66b556ea9b6190a3566c4f67428e9ebbf1068ef2
Md Saalim <113614361+saalim199@users.noreply.github.com>
2024-04-25 14:51:08 +0530

    Time: 3 ms (92.64%), Space: 12.1 MB (34.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int solve(int i, int j, string &s1, string &s2){
+    int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
         //base
         if(i<0) return j+1;
         if(j<0) return i+1;
-        if(s1[i]==s2[j]) return solve(i-1,j-1,s1,s2);
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]) return dp[i][j]=solve(i-1,j-1,s1,s2,dp);
         else{
-            int ins=solve(i,j-1,s1,s2);
-            int del=solve(i-1,j,s1,s2);
-            int rep=solve(i-1,j-1,s1,s2);
-            return 1+min(ins,min(del,rep));
+            int ins=solve(i,j-1,s1,s2,dp);
+            int del=solve(i-1,j,s1,s2,dp);
+            int rep=solve(i-1,j-1,s1,s2,dp);
+            return dp[i][j]=1+min(ins,min(del,rep));
         }
     }
     int minDistance(string s1, string s2) {
         if(s1.size()==0) return s2.size();
         if(s2.size()==0) return s1.size();
-        return solve(s1.size()-1,s2.size()-1,s1,s2);
-        // vector<vector<int>> dp(s1.size()+1,vector<int>(s2.size()+1,0));
+        vector<vector<int>> dp(s1.size()+1,vector<int>(s2.size()+1,-1));
+        return solve(s1.size()-1,s2.size()-1,s1,s2,dp);
         // for(int i=0;i<s1.size();i++){
         //     dp[i][0]=i;
         // }
