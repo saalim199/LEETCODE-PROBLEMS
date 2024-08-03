c186ec1e9baa46dbe22f714bde6473e668fa8655
Md Saalim <salimahmad199@gmail.com>
2024-08-03 18:22:30 +0530

    Time: 7 ms (61.26%), Space: 13 MB (8.73%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& grid,
-              vector<vector<int>>& dp) {
-        if (i == 0 && j == 0)
-            return grid[0][0];
-        if (i < 0 || j < 0)
-            return 1e8;
-        if (dp[i][j] != 1e8)
-            return dp[i][j];
-        int up = grid[i][j] + solve(i - 1, j, grid, dp);
-        int left = grid[i][j] + solve(i, j - 1, grid, dp);
-        return dp[i][j] = min(dp[i][j], min(up, left));
+    int solve(vector<vector<int>>& grid, int i, int j, vector<vector<int>> &dp){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()) return 1e8;
+        if(i==grid.size()-1 && j==grid[0].size()-1) return grid[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down=grid[i][j]+solve(grid,i+1,j,dp);
+        int right=grid[i][j]+solve(grid,i,j+1,dp);
+        return dp[i][j]=min(down,right);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), 1e8));
-        return solve(grid.size() - 1, grid[0].size() - 1, grid, dp);
+        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+        return solve(grid,0,0,dp);
     }
 };
