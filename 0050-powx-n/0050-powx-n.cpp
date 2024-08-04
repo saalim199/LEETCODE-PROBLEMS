5e05599bbd25b85ac23cccb2f8cb1660a2c7a6b0
Md Saalim <salimahmad199@gmail.com>
2024-08-04 13:47:59 +0530

    Time: 0 ms (100%), Space: 8 MB (83.6%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==0) return 1;
-       
+        if (n == 1)
+            return x;
             if(n%2==0)return myPow(x*x,n/2);
             else{
         if (n>0) {
