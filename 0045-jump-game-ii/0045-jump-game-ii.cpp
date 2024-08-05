b7dd30168f66ede7baab7e3860536a8ea731075f
Md Saalim <salimahmad199@gmail.com>
2024-08-05 14:01:31 +0530

    Time: 3 ms (99.06%), Space: 19.3 MB (50.37%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i){
+    //WITH DP SOLUTION
+    int solve(vector<int>& nums, int i, vector<int> &dp){
         if(i>=(nums.size()-1)) return 0;
+        if(dp[i]!=-1) return dp[i];
         int cnt=0;
         int mini=1e8;
         for(int j=1;j<=nums[i];j++){
-            cnt=1+solve(nums,i+j);
+            cnt=1+solve(nums,i+j,dp);
             mini=min(mini,cnt);
         }
-        return mini;
+        return dp[i]=mini;
     }
     int jump(vector<int>& nums) {
-        return solve(nums,0);
+        // vector<int> dp(nums.size(),-1);
+        // return solve(nums,0,dp);
+        int n=nums.size();
+        int index=0;
+        int maxi=0;
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            maxi=max(maxi,nums[i]+i);
+            if(i==index){
+                ans++;
+                index=maxi;
+                if(index>=n-1) return ans;
+            }
+        }
+        return ans;
     }
 };
