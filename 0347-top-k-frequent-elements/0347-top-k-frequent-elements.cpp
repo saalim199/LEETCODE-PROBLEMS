ec9fffd9a8c3edd004bdf30320ab7a48b2d15427
Md Saalim <salimahmad199@gmail.com>
2025-03-20 14:35:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        for(int &num:nums){
+            mp[num]++;
+        }
+        vector<int> ans;
+        for(auto &it:mp){
+            if(it.second>=k){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
