6f018732fa3d7ccfc4227207694af77fbfb3e07a
Md Saalim <salimahmad199@gmail.com>
2025-03-26 11:50:34 +0530

    Time: 0 ms (100%), Space: 19.7 MB (35.44%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -6,36 +6,32 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*, long long>> q;
-        if (root == NULL)
-            return 0;
-        q.push({root, 0});
-        int ans = 0;
-        while (!q.empty()) {
-            int size=q.size();
-            long long mini = q.front().second;
-            int first, last;
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front().first;
-                long long m = q.front().second - mini;
+        queue<pair<TreeNode*,long long>> q;
+        long long ans=0;
+        q.push({root,0});
+        while(!q.empty()){
+            int n=q.size();
+            long long mini=q.front().second;
+            // cout<<"MINI "<<mini<<endl;
+            long long maxi=0;
+            for(int i=0;i<n;i++){
+                TreeNode* node=q.front().first;
+                long long num=q.front().second;
                 q.pop();
-                if (i == 0)
-                    first = m;
-                if (i == size - 1)
-                    last = m;
-                if (node->left)
-                    q.push({node->left, (2 * m) + 1});
-                if (node->right)
-                    q.push({node->right, (2 * m )+ 2});
+                num-=mini;
+                // cout<<num<<" ";
+                if(i==n-1) maxi=num;
+                if(node->left) q.push({node->left,(2*num)+1});
+                if(node->right) q.push({node->right,(2*num)+2});
             }
-            ans = max(ans, (last - first) + 1);
+            // cout<<endl;
+            ans=max(ans,maxi+1);
         }
         return ans;
     }
