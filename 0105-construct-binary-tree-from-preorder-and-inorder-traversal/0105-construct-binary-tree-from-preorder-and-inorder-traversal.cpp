4cc0e62e16b21cbb7dc9ca30030af18e0ba35f57
Md Saalim <salimahmad199@gmail.com>
2025-03-30 11:47:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,22 +10,37 @@
  * };
  */
 class Solution {
-private:
-    TreeNode* build(vector<int>& pre,int preStart,int preEnd,vector<int>& in,int inStart,int inEnd, unordered_map<int,int> &inMap){
-        if(preStart>preEnd || inStart>inEnd) return NULL;
-        TreeNode *root=new TreeNode(pre[preStart]);
-        int inRoot=inMap[root->val];
-        int x=inRoot-inStart;
-        root->left=build(pre,preStart+1,preStart+x,in,inStart,inRoot-1,inMap);
-        root->right=build(pre,preStart+x+1,preEnd,in,inRoot+1,inEnd,inMap);
-        return root;
-    }
 public:
-    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
-        unordered_map<int,int> inMap;
-        for(int i=0;i<in.size();i++){
-            inMap[in[i]]=i;
-        }
-        return build(pre,0,pre.size()-1,in,0,in.size()-1,inMap);
+    void makeTree(TreeNode* &root, vector<int>& preorder, vector<int>& inorder){
+        TreeNode* node=new TreeNode();
+        node->val=preorder[0];
+        root=node;
+        // cout<<root->val<<" ";
+        if(preorder.size()>1){
+            // int found=0;
+            int cnt=0;
+            for(int i=0;i<inorder.size();i++){
+                if(inorder[i]==preorder[0]){
+                    // found=i;
+                    break;
+                }
+                else cnt++;
+            }
+            // vector<int> newPre = ;
+            // vector<int> newIn = ;
+            int n=preorder.size();
+            cnt++;
+            vector<int> leftPre(preorder.begin()+1,preorder.begin()+cnt);
+            vector<int> leftIn(inorder.begin(),inorder.begin()+cnt);
+            vector<int> rightPre(preorder.begin()+cnt,preorder.end());
+            vector<int> rightIn(inorder.begin()+cnt,inorder.end());
+            makeTree(root->left,leftPre,leftIn);
+            makeTree(root->right,rightPre,rightIn);
+        } 
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        TreeNode* node=new TreeNode();
+        makeTree(node,preorder,inorder);
+        return node; 
     }
 };
