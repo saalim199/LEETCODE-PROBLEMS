6ef5fd6d95de252cdca1edf6e0b041a4f6284fa1
Md Saalim <salimahmad199@gmail.com>
2025-04-11 18:41:40 +0530

    Time: 62 ms (5.05%), Space: 125.4 MB (5.05%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    bool dfs(int i, vector<int> adj[], vector<int> &vis, vector<int> &visPath){
+    bool dfs(int i, vector<int> adj[], vector<int> &vis, vector<int> pathVis){
+        cout<<i<<" ";
         vis[i]=1;
-        visPath[i]=1;
-        for(auto node: adj[i]){
-            if(!vis[node]){
-                if(dfs(node,adj,vis,visPath)) return true;
-            }
-            else if(visPath[node]) return true;
+        pathVis[i]=1;
+        for(auto a:adj[i]){
+            if(!vis[a]){
+                if(dfs(a,adj,vis,pathVis)){
+                    return true;
+                }
+            }else if(pathVis[a]) return true;
         }
-        visPath[i]=0;
+        pathVis[i]=0;
         return false;
     }
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<int> adj[n];
-        for(int i=0;i<pre.size();i++){
-            adj[pre[i][1]].push_back(pre[i][0]);
+        for(auto a:pre){
+            adj[a[1]].push_back(a[0]);
         }
         vector<int> vis(n,0);
-        vector<int> visPath(n,0);
+        vector<int> pathVis(n,0);
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                if(dfs(i,adj,vis,visPath)) return false;
+                if(dfs(i,adj,vis,pathVis)) return false;
             }
         }
         return true;
