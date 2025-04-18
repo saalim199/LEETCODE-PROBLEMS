01ec98951ac9a6536ad79f3f7b6e6a8126e4c946
Md Saalim <salimahmad199@gmail.com>
2025-04-18 10:53:10 +0530

    Time: 6 ms (34.47%), Space: 9.8 MB (82.8%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     string countAndSay(int n) {
         string s="1";
-        for(int i=1;i<n;i++){
-                string temp="";
+        for (int i = 1; i < n; i++) {
+            string temp="";
             for(int j=0;j<s.size();j++){
                 int cnt=1;
-                char ch=s[j];
-                while((j+1)<s.size() && s[j]==s[j+1]){
+                while(j+1<s.size() && s[j]==s[j+1]){
                     cnt++;
                     j++;
                 }
-                string a =to_string(cnt);
-                temp+=a;
-                temp.push_back(ch);
+                temp+=to_string(cnt);
+                temp+=s[j];
             }
             s=temp;
-            cout<<s<<endl;
         }
         return s;
     }
