38591177f4c0fe7ab6d77c82a4c8d4e175019ee6
Md Saalim <salimahmad199@gmail.com>
2025-04-21 22:06:35 +0530

    Time: 26 ms (63%), Space: 27.4 MB (50.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    // int solve(string& t1, string& t2, int i, int j, vector<vector<int>>& dp) {
-    //     if (i < 0 || j < 0)
-    //         return 0;
-    //     if (dp[i][j] != -1)
-    //         return dp[i][j];
-    //     if (t1[i] == t2[j])
-    //         dp[i][j] = 1 + solve(t1, t2, i - 1, j - 1, dp);
-    //     else
-    //         dp[i][j] =
-    //             max(solve(t1, t2, i, j - 1, dp), solve(t1, t2, i - 1, j, dp));
-    //     return dp[i][j];
-    // }
-    int longestCommonSubsequence(string t1, string t2) {
-        // vector<vector<int>> dp(t1.size()+1, vector<int>(t2.size()+1, 0));
-        vector<int> prev(t2.size()+1,0);
-        for(int i=0;i<t1.size();i++){
-            vector<int> curr(t2.size()+1,0);
-            for(int j=0;j<t2.size();j++){
-                if(t1[i]==t2[j]){
-                    curr[j+1]=prev[j]+1;
-                }else{
-                    curr[j+1]=max(prev[j+1],curr[j]);
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(),0));
+        for(int i=0;i<text2.size();i++){
+            if(text1[0]==text2[i]) dp[0][i]=1;
+            else if(i!=0 && dp[0][i-1]==1) dp[0][i]=1;
+        }
+        for(int i=0;i<text1.size();i++){
+            if(text2[0]==text1[i]) dp[i][0]=1;
+            else if(i!=0 && dp[i-1][0]==1) dp[i][0]=1;
+        }
+        for(int i=1;i<text1.size();i++){
+            for(int j=1;j<text2.size();j++){
+                if(text1[i]==text2[j]) dp[i][j]=dp[i-1][j-1]+1;
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
-            prev=curr;
         }
-        return prev[t2.size()];
-        // return solve(t1, t2, t1.size() - 1, t2.size() - 1, dp);
+        // for(int i=0;i<text1.size();i++){
+        //     for(int j=0;j<text2.size();j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[text1.size()-1][text2.size()-1];
     }
 };
