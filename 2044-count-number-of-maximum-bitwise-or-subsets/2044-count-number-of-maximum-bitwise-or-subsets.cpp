c2b142328bdf3a1d5e36740c8b91a521b84f0960
Md Saalim <salimahmad199@gmail.com>
2025-07-28 11:41:30 +0530

    Time: 27 ms (44.81%), Space: 11.1 MB (36.23%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    void solve(int i, vector<int>& nums, vector<int>& sub, unordered_map<int,int>& orMap){
+    void solve(int i, vector<int>& nums, vector<int>& sub, int& res, int& maxi){
         if(i>=nums.size()){
             int sum=0;
             for(auto &s:sub){
                 sum|=s;
             }
-            orMap[sum]++;
+            if(sum==maxi) res++;
             return;
         }
-        solve(i+1,nums,sub,orMap);
+        solve(i+1,nums,sub,res,maxi);
         sub.push_back(nums[i]);
-        solve(i+1,nums,sub,orMap);
+        solve(i+1,nums,sub,res,maxi);
         sub.pop_back();
     }
     int countMaxOrSubsets(vector<int>& nums) {
         vector<int> sub;
-        unordered_map<int,int> orMap;
         int maxi=0;
-        int res=0;
-        solve(0,nums,sub,orMap);
-        for(auto &[x,y]:orMap){
-            // cout<<x<<" "<<y<<endl;
-            if(x>maxi){
-                maxi=x;
-                res=y;
-            }
+        for(auto &n:nums){
+            maxi|=n;
         }
+        int res=0;
+        solve(0,nums,sub,res,maxi);
         return res;
     }
 };
