6042a796fa17f474074a9bc13e2270671dcbef26
Md Saalim <salimahmad199@gmail.com>
2026-01-12 14:54:55 +0530

    Time: 3 ms (62.9%), Space: 10.2 MB (75.94%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,38 +1,58 @@
 class Solution {
 public:
-    void solve(int row,vector<vector<string>> &ans,vector<string> &board,vector<int> &ROW,vector<int> &COL,vector<int> &UPPER,vector<int> &LOWER,int n){
-        if(row==n){
+    bool checkValid(vector<string> &board, int row, int col){
+        int n=board.size();
+        //check right of the row
+        for(int i=col+1;i<n;i++){
+            if(board[row][i]=='Q') return false;
+        }
+        //check left of the row
+        for(int i=col-1;i>=0;i--){
+            if(board[row][i]=='Q') return false;
+        }
+        //check top of the col
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q') return false;
+        }
+        //check bottom col
+        for(int i=row+1;i<n;i++){
+            if(board[i][col]=='Q') return false;
+        }
+        //diagonal up right
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q') return false;
+        }
+        //diagonal up left
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q') return false;
+        }
+        //diagonal down right
+        for(int i=row+1,j=col+1;i<n && j<n;i++,j++){
+            if(board[i][j]=='Q') return false;
+        }
+        //diagonal up left
+        for(int i=row+1,j=col-1;i<n && j>=0;i++,j--){
+            if(board[i][j]=='Q') return false;
+        }
+        return true;
+    }
+    void helper(vector<string> &board, vector<vector<string>> &ans, int index){
+        if(index==board.size()){
             ans.push_back(board);
             return;
         }
-            for(int j=0;j<n;j++){
-                if(ROW[row]==0 && COL[j]==0 && UPPER[row+j]==0 && LOWER[(n-1)+j-row]==0){
-                    ROW[row]=1;
-                    COL[j]=1;
-                    UPPER[row+j]=1;
-                    LOWER[(n-1)+j-row]=1;
-                    board[row][j]='Q';
-                    solve(row+1,ans,board,ROW,COL,UPPER,LOWER,n);
-                    ROW[row]=0;
-                    COL[j]=0;
-                    UPPER[row+j]=0;
-                    LOWER[(n-1)+j-row]=0;
-                    board[row][j]='.';
-                }
+        for(int i=0;i<board.size();i++){
+            if(checkValid(board,index,i)){
+                board[index][i]='Q';
+                helper(board,ans,index+1);
+                board[index][i]='.';
             }
+        }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board;
-        string s(n,'.');
-        for(int i=0;i<n;i++){
-            board.push_back(s);
-        }
-        vector<int> ROW(n,0);
-        vector<int> COL(n,0);
-        vector<int> UPPER(2*n-1,0);
-        vector<int> LOWER(2*n-1,0);
-        solve(0,ans,board,ROW,COL,UPPER,LOWER,n);
+        vector<string> board(n,string(n,'.'));
+        helper(board,ans,0);
         return ans;
     }
 };
